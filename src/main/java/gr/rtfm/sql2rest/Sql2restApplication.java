d6163f84a9d51b925d3c8308e1290d5f6dc61115
Lefty Kreouzis <lefty.kreouzis@rtfm.gr>
2025-02-26 06:31:13 +0400

    Initial Commit

---
+++ src/main/java/gr/rtfm/sql2rest/Sql2restApplication.java
@@ -0,0 +1,13 @@
+package gr.rtfm.sql2rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sql2restApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sql2restApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/rtfm/sql2rest/Sql2restApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.rtfm.sql2rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sql2restApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
